5eaee6e32907878c84e68360cc0b37b72b21b406
Zoltan Kalanovics <kalanovicszoltan@gmail.com>
2020-12-09 13:42:32 +0100

    Added comments and acceptance criteria

--- selenium/automated_1.py
+++ selenium/automated_1.py
@@ -1,6 +1,7 @@
 # Acceptance test #1 
-#This test fill the form correctly and submitting it
-#
+#  Acceptance criteria: You can submit the form after filling the form with valid data
+# With this test you can test if the form is getting submitted properly
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,10 +10,12 @@ import time
 class LiferayForms(unittest.TestCase):
 
     def setUp(self):
+        #Open up the browser
         self.driver = webdriver.Chrome()
 
     def test_acceptance(self):
         driver = self.driver
+        #Getting the URL for the form
         driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548")
         name = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[1]/div/div/div[1]/div[1]/div/input")
         name.send_keys("John Smith")
@@ -22,11 +25,14 @@ class LiferayForms(unittest.TestCase):
         text = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/textarea")
         text.send_keys("some random text")
         submit = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[2]/button").click()
-        time.sleep(10)
+        time.sleep(2)
+        #Checking if the form is being submitted successfully
+        assert "Form - Forms" in driver.title
         
         
     
     def tearDown(self):
+        #Closing the browser after the test run
         self.driver.close()
 
 
--- selenium/automated_2.py
+++ selenium/automated_2.py
@@ -1,6 +1,7 @@
-# Acceptance test #1 
-#You can change the langugae to portuguese
-#
+# Acceptance test #2 
+# Acceptance criteria: You can change the langugae to portuguese
+# With this test you can check if the language change is working properly
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,25 +10,24 @@ import time
 class LiferayForms(unittest.TestCase):
 
     def setUp(self):
+        #Open up the browser
         self.driver = webdriver.Chrome()
 
     def test_acceptance(self):
         driver = self.driver
+        #Getting the URL for the form
         driver.get("https://forms.liferay.com/web/forms/shared/-/form/122548")
         language = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[1]/div/div/div/button").click()
         portuguese = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/ul/li/a").click()
-        # name = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[1]/div/div/div[1]/div[1]/div/input")
-        # name.send_keys("John Smith")
-        # calender = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[1]/div/div/div[1]/div[2]/div/div/div[1]/div/button").click()
-        # birthdate = driver.find_element_by_xpath("//div[contains(text(),'10')]").click()
-        # text = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[1]/div/div/div[2]/div/div/textarea")
-        # text.send_keys("some random text")
-        # submit = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/div/div/form/div[3]/div/div/div/div[2]/button").click()
-        time.sleep(10)
+        time.sleep(2)
+        #Checking for the language change
+        assert "Este é um Liferay Forms" in driver.title
+        
         
         
     
     def tearDown(self):
+        #Closing the browser after the test run
         self.driver.close()
 
 
